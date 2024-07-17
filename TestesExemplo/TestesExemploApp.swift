730d6eb7dfdf4bf338383a91ff128102b3ee11b9
Lais Godinho <47223072+laisbastosbg@users.noreply.github.com>
2024-07-17 11:32:45 -0300

    Initial Commit

---
+++ TestesExemplo/TestesExemploApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestesExemploApp.swift
+//  TestesExemplo
+//
+//  Created by Lais Godinho on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TestesExemploApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
